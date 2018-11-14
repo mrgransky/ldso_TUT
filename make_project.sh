3d1a0f205af953ec5709ccff6f2e1c57472e638e
Farid Alijani <farid.alijani@gmail.com>
2018-11-14 18:31:38 +0200

    files up

--- make_project.sh
+++ make_project.sh
@@ -10,24 +10,24 @@ BASEDIR="$PWD"
 cd "$BASEDIR/thirdparty/DBoW3"
 rm -rf build/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
 cd "$BASEDIR/thirdparty/g2o"
 rm -rf build/ lib/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
 cd "$BASEDIR"
 rm -rf build/ bin/ lib/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
